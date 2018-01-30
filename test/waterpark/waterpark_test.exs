676d4e894dcb180f19166934dcce9e5ef99ab9a5
Will Ferguson <wferguson@northplains.com>
2018-01-30 09:07:23 +0000

    Uses Waterpark facade

--- test/waterpark/waterpark_test.exs
+++ test/waterpark/waterpark_test.exs
@@ -7,10 +7,10 @@ defmodule Waterpark.WaterparkTest do
   end
 
   test "Start test worker" do
-    Waterpark.Owner.create_pool("test_pool", 3, {Waterpark.TestWorker, :start_link, []})
-    {:ok, pid1} = Waterpark.Pool.run("test_pool", [[]])
-    {:ok, pid2} = Waterpark.Pool.run("test_pool", ["2"])
-    {:ok, pid3} = Waterpark.Pool.run("test_pool", ["3"])
-    {:error, _reason} = Waterpark.Pool.run("test_pool", ["too_much"])
+    Waterpark.create_pool("test_pool", 3, {Waterpark.TestWorker, :start_link, []})
+    {:ok, pid1} = Waterpark.run("test_pool", [[]])
+    {:ok, pid2} = Waterpark.run("test_pool", ["2"])
+    {:ok, pid3} = Waterpark.run("test_pool", ["3"])
+    {:error, _reason} = Waterpark.run("test_pool", ["too_much"])
   end
 end
